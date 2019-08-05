9ba63b52474477d6c2e5bc76bbf4e9efdaf8c044
rahulmala007 <48763412+rahulmala007@users.noreply.github.com>
2019-08-05 22:59:30 +0530

    Moved all to lab 1 and added lab2

---
+++ Lab 2/list.cpp
@@ -0,0 +1,175 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+	int x;
+	int y;
+	Node* next;
+};
+
+float dist(int x,int y)
+{
+	return sqrt(x*x+y*y);
+}
+Node* head=NULL;
+Node* newnode(int x,int y)
+{
+	Node* node=(Node*)malloc(sizeof(Node));
+	node->x=x;
+	node->y=y;
+	node->next=NULL;
+	return node;
+}
+
+void AddFirst(int x,int y)
+{
+
+	Node* node= newnode(x,y);
+
+	if(head==NULL) 
+	{
+			head=node;
+			return ;
+	}
+	node->next=head;
+	head=node;
+	
+
+
+}
+
+void DelFirst()
+{
+	if(head==NULL) 
+	{
+		cout<<"-1"<<endl;
+		return;
+	}
+	Node* x=head->next;
+	free(head);
+	head=x;
+
+}
+
+void Del(int x,int y)
+{	Node * temp;
+	if(head==NULL) 
+	{
+		cout<<"-1"<<endl;
+		return;	
+	}
+	Node* prev=head;
+	if(prev->x==x && prev->y==y) 
+	{
+		DelFirst();
+		return;
+	}
+	int check=0;
+	while(prev!=NULL)
+	{	temp=prev->next;
+		if(temp!=NULL)
+		{
+
+			if(temp->x==x && temp->y==y) {
+			check=1;
+			prev->next=temp->next;
+			free(temp);
+											}
+			else prev=prev->next;
+		}
+		
+	}
+	if(check==0) cout<<"-1"<<endl;
+	return;
+
+}
+
+void Search(float d,Node* temp)
+{
+	if(temp==NULL) return;
+
+	if(dist(temp->x,temp->y)<=d) 
+		{	cout<<"("<<temp->x<<","<<temp->y<<")"<<" ";
+			Search(d,temp->next);
+			return ;
+		}
+	else{
+		Search(d,temp->next);
+		return;
+	}	
+}
+
+bool Search(int x,int y)
+{
+	Node* temp=head;
+	if (head==NULL) return false;
+
+	while(temp!=NULL)
+	{
+
+		if(temp->x==x && temp->y==y) return true;
+		else temp =temp->next;
+	}
+	return false;
+}
+
+
+int length()
+{
+	if(head==NULL) return 0;
+	Node* temp=head;
+	int l=0;
+	while(temp!=NULL)
+	{
+		l++;
+		temp=temp->next;
+	}
+	return l;
+}
+
+
+int main()
+{
+	long int t;
+	cin>>t;
+
+	while(t--)
+	{
+		int a,b,c;
+		float d;	
+		cin>>a;
+		if(a==1)
+		{
+			cin>>b;
+			cin>>c;
+			AddFirst(b,c);
+		}
+		else if(a==2)
+		{
+				DelFirst();
+		}
+		else if(a==3)
+		{
+				cin>>b>>c;
+				Del(b,c);
+		}
+		else if(a==4)
+		{
+				cin>>d;
+				Search(d,head);
+		}
+		else if(a==5)
+		{
+			cin>>b>>c;
+			if(Search(b,c)) cout<<"True"<<endl;
+			else cout<<"False"<<endl;
+		}
+		else
+		{
+			cout<<length()<<endl;
+		}
+
+
+	}
+
+}
